9fd2cf7033bc2f044bb0ddb85a92487cd463bfc2
Gerald Villorente <gerald@vielsoft.com>
2017-07-20 23:41:12 +0800

    Added class for APIs

--- app/sites/all/modules/custom/bullseye_api/bullseye_api.api.php
+++ app/sites/all/modules/custom/bullseye_api/bullseye_api.api.php
@@ -2,3 +2,16 @@
 /**
  * Bullseye API file.
  */
+
+class Bullseye {
+  /**
+   * Initialize properties for the class.
+   *
+   * @param object $user
+   *   An object containing the data of the current logged in user.
+   */
+  function __construct($user) {
+    $this->user = $user;
+    $this->uid = $user->uid;
+  }
+}
