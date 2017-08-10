932820c62f23d65203c4992f9b0def98652e04e6
Ruthie Hallarsis <ruthieborces@outlook.com>
2017-08-10 23:46:44 +0800

    Disabled tooltips

--- app/sites/all/modules/custom/bullseye_blocks/templates/be-leads.tpl.php
+++ app/sites/all/modules/custom/bullseye_blocks/templates/be-leads.tpl.php
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col-md-6">
         <span class="account-count"><?php print t('All Leads (50)'); ?></span>
-        <a class="be-table-header-button" href="/node/add/accounts" rel="lightframe"><?php print t('Add New'); ?></a>
+        <a class="be-table-header-button" href="/node/add/accounts?account_status=lead" rel="lightframe"><?php print t('Add New'); ?></a>
         <a class="be-table-header-button" href="/admin/content/leads/import" rel="lightframe"><?php print t('Import'); ?></a>
       </div>
       <div class="col-md-6"></div>
