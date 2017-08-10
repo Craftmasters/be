d28b1272a19977cc2ff60c16e4d4d0f8c6df6585
Gerald Villorente <gerald@vielsoft.com>
2017-08-10 17:27:45 +0800

    Added custom module to allow BE to fetch and display Gmail emails in BE space.

---
+++ app/sites/all/modules/custom/gmail_connector/README.txt
@@ -0,0 +1,69 @@
+CONTENTS OF THIS FILE
+---------------------
+ * Description
+ * Introduction
+ * Installing
+ * Uninstalling
+ * Frequently Asked Questions (FAQ)
+ * Known Issues
+ * More Information
+
+
+DESCRIPTION
+-----------
+Gmail Connector helps to intergrate the gmail with any existing drupal instance.
+
+INTRODUCTION
+------------
+
+Current Maintainer: Mohammed Imran <https://www.drupal.org/u/ajay-gadhavana>
+
+Gmail Connector helps to intergrate the gmail with any existing drupal instance.
+
+INSTALLING
+----------
+
+See http://drupal.org/getting-started/install-contrib for instructions on
+how to install or update Drupal modules.
+
+Once Gmail Connector  module is installed and enabled,
+you can adjust the settings for your
+admin/config/gmail-connector
+
+1.enable Gmail API access to get our API credentials.
+2.visit Google’s Developer Console in our favourite web browser.
+3.create a project (or choose an existing one) and go to the APIs section.
+4.Choose “Gmail API” under the Google Apps APIs section and
+click the “Enable API” button.
+
+Now we need to create two sets of credentials,
+one for an OAuth 2.0 client ID for a web application and
+the other to create a browser API key.
+This can be done in the credentials section of the Google Developer Console
+by clicking the “Add Credentials” button.
+
+
+UNINSTALLING
+------------
+
+You can easily uninstall module from module list, there is no any dependency
+
+
+FREQUENTLY ASKED QUESTIONS (FAQ)
+--------------------------------
+
+- There are no frequently asked questions at this time.
+
+
+KNOWN ISSUES
+------------
+
+Error - Error at the place of button
+Solution :  Please confirm that you placed right channel name
+or channel id in configuration page
+
+
+MORE INFORMATION
+----------------
+
+- There are no more information.
---
+++ app/sites/all/modules/custom/gmail_connector/js/utilities.js
@@ -0,0 +1,284 @@
+/**
+ * @file
+ * Javascript file for the Gmail Connector.
+ *
+ * Javascript file for the Google Connector.
+ */
+
+/* global Drupal Pace gapi window to:true body:true subject:true btoa jQuery alert setTimeout data_length:true */
+/* eslint strict: [2, "never"]*/
+
+(function ($) {
+    Drupal.behaviors.gmail_connector = {
+      attach: function (context, settings) {
+            Pace.start();
+            var dataSet = [];
+            var mailBody = [];
+            // Load the client for javascript API.
+            $.getScript(
+                'https://apis.google.com/js/client.js', function () {
+                      gapi.load('auth:client', handleClientLoad);
+                    }
+            );
+
+            // Google Javascript API Calls.
+            var clientId = Drupal.settings.gmail_connector.config.google_client_id;
+            var apiKey = Drupal.settings.gmail_connector.config.google_api_key;
+            // Var scopes = 'https://www.googleapis.com/auth/gmail.readonly';
+            var scopes = 'https://mail.google.com/';
+
+            function handleClientLoad() {
+              gapi.client.setApiKey(apiKey);
+              window.setTimeout(checkAuth, 1);
+            }
+
+            /**
+             * Check if current user has authorized this application.
+             */
+
+            function checkAuth() {
+              gapi.auth.authorize(
+                {
+                  client_id: clientId,
+                  scope: scopes,
+                  immediate: true
+                }, handleAuthResult
+                );
+            }
+
+            /**
+             * Initiate auth flow in response to user clicking authorize button.
+             *
+             * @param {Event} event Button click event.
+             */
+
+            function handleAuthClick() {
+              gapi.auth.authorize(
+                {
+                  client_id: clientId,
+                  scope: scopes,
+                  immediate: false
+                }, handleAuthResult
+                );
+              return false;
+            }
+
+            /**
+             * Handle response from authorization server.
+             *
+             * @param {Object} authResult Authorization result.
+             */
+
+            function handleAuthResult(authResult) {
+              if (authResult && !authResult.error) {
+                loadGmailApi();
+                $('#authorize-button').remove();
+                $('#google-inbox_wrapper').show();
+                $('#send-button').removeClass("hidden");
+                $('#email-send').on(
+                        'click', function () {
+                            var userId = 'me';
+                            var email = 'imran031387@gmail.com';
+                            to = $("input#email-to").val();
+                            body = $("#email-body").val();
+                            subject = $("#email-subject").val();
+
+                            sendMessage(userId, email, sendMessageSuccess);
+                          }
+                    );
+
+              }
+                else {
+                $('#authorize-button').removeClass("hidden");
+                $('#authorize-button').on(
+                        'click', function () {
+                          handleAuthClick();
+                        }
+                  );
+              }
+            }
+
+            /**
+             * Load Gmail API client library. List labels once client library is loaded.
+             */
+
+            function loadGmailApi() {
+              gapi.client.load('gmail', 'v1', displayInbox);
+            }
+
+            function displayInbox() {
+              var request = gapi.client.gmail.users.messages.list(
+                {
+                  userId: 'me',
+                  labelIds: 'INBOX',
+                  maxResults: Drupal.settings.gmail_connector.config.google_email_display
+                }
+                );
+              request.execute(
+                    function (response) {
+                        data_length = response.messages.length;
+                        $.each(
+                            response.messages, function () {
+                                var messageRequest = gapi.client.gmail.users.messages.get(
+                                  {
+                                    userId: 'me',
+                                    id: this.id
+                                  }
+                                );
+
+                                messageRequest.execute(appendMessageRow);
+                              }
+                        );
+                      }
+                );
+            }
+
+            function appendMessageRow(message) {
+               // console.log(message);
+                // console.log('length ::: '+dataSet.length);.
+              var del_btn = '<button id="' + 'delete-button-' + message.id + '" class="btn btn-primary email-delete" data-id="' + message.id + '">Delete</button>';
+              var mail_link = '<a href="#message-modal-' + message.id +
+                          '" data-toggle="modal" id="message-link-' + message.id + '" data-lid="' + message.id + '">' +
+                          getHeader(message.payload.headers, 'Subject') +
+                          '</a>';
+              var dataArray = [getHeader(message.payload.headers, 'From'), mail_link, getHeader(message.payload.headers, 'Date'), del_btn];
+              dataSet.push(dataArray);
+                // Mail body Array.
+              mailBody[message.id] = getBody(message.payload);
+
+              $('body').append(
+                    '<div class="modal fade" id="message-modal-' + message.id +
+                    '" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" >\n' +
+                  '<div class="modal-dialog modal-lg"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>\n' +
+                        '<h4 class="modal-title" id="myModalLabel">' +
+                    getHeader(message.payload.headers, 'Subject') +
+                    '</h4></div><div class="modal-body"><iframe id="message-iframe-' + message.id + '" srcdoc="<p>Loading...</p>"></iframe>\n' +
+                '</div></div></div></div>'
+                );
+
+              if (dataSet.length === data_length) {
+                $('#google-inbox').DataTable(
+                  {
+                    data: dataSet,
+                    columns: [
+                      {title: "From"},
+                      {title: "Subject"},
+                      {title: "Date/Time"},
+                      {title: "Actions"}
+                    ]
+                  }
+                    );
+
+                Pace.stop();
+                $('[id^="delete-button-"]').on(
+                        'click', function () {
+                            deleteMessage('me', $(this).data("id"));
+                          }
+                    );
+
+                $('[id^="message-link-"]').on(
+                        'click', function () {
+                            var ifrm = $('#message-iframe-' + $(this).data("lid"))[0].contentWindow.document;
+                            $('body', ifrm).html(mailBody[$(this).data("lid")]);
+                          }
+                    );
+              }
+            }
+
+            function getHeader(headers, index) {
+              var header = '';
+              $.each(
+                    headers, function () {
+                        if (this.name === index) {
+                          header = this.value;
+                        }
+                      }
+                );
+              return header;
+            }
+
+            function getBody(message) {
+              var encodedBody = '';
+              if (typeof message.parts === 'undefined') {
+                encodedBody = message.body.data;
+              }
+                else {
+                encodedBody = getHTMLPart(message.parts);
+              }
+              encodedBody = encodedBody.replace(/-/g, '+').replace(/_/g, '/').replace(/\s/g, '');
+              return decodeURIComponent(escape(window.atob(encodedBody)));
+            }
+
+            function getHTMLPart(arr) {
+              for (var x = 0; x <= arr.length; x++) {
+                if (typeof arr[x].parts === 'undefined') {
+                  if (arr[x].mimeType === 'text/html') {
+                    return arr[x].body.data;
+                  }
+                }
+                  else {
+                  return getHTMLPart(arr[x].parts);
+                }
+              }
+              return '';
+            }
+
+            /**
+             * Send Message.
+             *
+             * @param {String} userId User's email address. The special value 'me'
+             * can be used to indicate the authenticated user.
+             * @param {String} email RFC 5322 formatted String.
+             * @param {Function} sendMessageSuccess Function to call when the request is complete.
+             */
+            function sendMessage(userId, email, sendMessageSuccess) {
+              var base64EncodedEmail = btoa(
+                    "Content-Type:  text/plain; charset=\"UTF-8\"\n Content-length: 5000\n Content-Transfer-Encoding: message/rfc2822\n" +
+                    "to: " + to + "\n" +
+                    "subject: " + subject + "\n\n" + body
+                ).replace(/\+/g, '-').replace(/\//g, '_');
+              var request = gapi.client.gmail.users.messages.send(
+                {
+                  userId: 'me',
+                  resource: {
+                    raw: base64EncodedEmail
+                  }
+                }
+                );
+              request.execute(sendMessageSuccess);
+            }
+
+            function sendMessageSuccess() {
+              $('#model-email-success').show();
+              setTimeout(emailSuccess, 1000);
+            }
+
+            function emailSuccess() {
+              $('#model-email-success').hide();
+              $('#message-modal-compose').modal('hide');
+            }
+
+            /**
+             * Delete Message with given ID.
+             *
+             * @param {String} userId User's email address. The special value 'me'
+             * can be used to indicate the authenticated user.
+             * @param {String} messageId ID of Message to delete.
+             */
+            function deleteMessage(userId, messageId) {
+              var request = gapi.client.gmail.users.messages.delete(
+                {
+                  userId: userId,
+                  id: messageId
+                }
+                );
+              request.execute(
+                    function (resp) {
+                        alert('Message Successfully Deleted');
+                        window.location.reload();
+                      }
+                );
+            }
+          }
+    };
+  })(jQuery);
