f4ecc8991631314fbcceb0caa8e02a1e76e7445c
Ruthie Hallarsis <ruthieborces@outlook.com>
2017-08-19 18:11:53 +0800

    Completed markup for people block

--- app/sites/all/modules/custom/bullseye_blocks/templates/be-people.tpl.php
+++ app/sites/all/modules/custom/bullseye_blocks/templates/be-people.tpl.php
@@ -1,3 +1,40 @@
 <div class="be-regular-block">
   <h2 class="be-regular-h2"><?php print t('People'); ?></h2>
+  <a href="#" class="edit-link edit-account-details"><img src="<?php print $edit_icon; ?>"></a>
+  <div class="be-block-main">
+
+  	<div class="be-user-row">
+  		<div class="be-user-img">
+  			<img src="/sites/all/themes/bullseye/images/default-user.png">
+  		</div>
+  		<div class="be-user-name">Chris Martin</div>
+  		<div class="be-user-position">Chief Executive Officer</div>
+  		<div class="be-user-contact">
+  			<span class="phone">(880) 827-3959</span> | <a href="mailto:chris@abccompany.com" class="be-user-email">chris@abccompany.com</a>
+  		</div>
+  	</div>
+
+  	<div class="be-user-row">
+  		<div class="be-user-img">
+  			<img src="/sites/all/themes/bullseye/images/default-user.png">
+  		</div>
+  		<div class="be-user-name">Jack James</div>
+  		<div class="be-user-position">Chief Financial Officer</div>
+  		<div class="be-user-contact">
+  			<span class="phone">(880) 827-3959</span> | <a href="mailto:chris@abccompany.com" class="be-user-email">jack@abccompany.com</a>
+  		</div>
+  	</div>
+
+  	<div class="be-user-row">
+  		<div class="be-user-img">
+  			<img src="/sites/all/themes/bullseye/images/default-user.png">
+  		</div>
+  		<div class="be-user-name">Jon Lee</div>
+  		<div class="be-user-position">HR Director</div>
+  		<div class="be-user-contact">
+  			<span class="phone">(880) 827-3959</span> | <a href="mailto:chris@abccompany.com" class="be-user-email">jon@abccompany.com</a>
+  		</div>
+  	</div>
+
+  </div>
 </div>
