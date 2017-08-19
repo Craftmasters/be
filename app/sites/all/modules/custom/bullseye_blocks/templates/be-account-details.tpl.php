512809848c548ac7fb13a919a30637589506f9c0
Ruthie Hallarsis <ruthiehallarsis@Ruthies-MacBook-Pro.local>
2017-08-19 17:13:38 +0800

    Completed markup for account details

--- app/sites/all/modules/custom/bullseye_blocks/templates/be-account-details.tpl.php
+++ app/sites/all/modules/custom/bullseye_blocks/templates/be-account-details.tpl.php
@@ -1,3 +1,76 @@
 <div class="be-regular-block">
   <h2 class="be-regular-h2"><?php print t('Account Details'); ?></h2>
+  <a href="#" class="edit-link edit-account-details"><img src="$edit_icon"></a>
+  <div class="be-block-main">
+
+  	<div class="be-view-field">
+  		<div class="be-view-label"><?php print t('Company Name'); ?></div>
+	  	<div class="be-view-value">ABC Company</div>
+  	</div>
+	  
+	  <div class="be-view-field">
+  		<div class="be-view-label"><?php print t('Primary Email Address'); ?></div>
+	  	<div class="be-view-value"><a href="mailto:info@abccompany.com">info@abccompany.com</a></div>
+  	</div>
+
+  	<div class="be-view-field">
+  		<div class="be-view-label"><?php print t('Phone Number'); ?></div>
+	  	<div class="be-view-value"><a href="tel:4513554776">(451) 355-4776</a></div>
+  	</div>
+
+  	<div class="be-view-field">
+  		<div class="be-view-label"><?php print t('Website'); ?></div>
+	  	<div class="be-view-value"><a href="#">https://abccompany.com</a></div>
+  	</div>
+
+  	<div class="be-view-field">
+  		<div class="be-view-label"><?php print t('Company Street Address'); ?></div>
+	  	<div class="be-view-value">888 19th Street</div>
+  	</div>
+
+  	<div class="be-view-field-row row">
+
+  		<div class="col-xs-4">
+  			<div class="be-view-field">
+		  		<div class="be-view-label"><?php print t('City'); ?></div>
+			  	<div class="be-view-value">San Antonio</div>
+		  	</div>
+	  	</div>
+
+  		<div class="col-xs-4">
+  			<div class="be-view-field">
+		  		<div class="be-view-label"><?php print t('State'); ?></div>
+			  	<div class="be-view-value">TX</div>
+		  	</div>
+	  	</div>
+
+	  	<div class="col-xs-4">
+  			<div class="be-view-field">
+		  		<div class="be-view-label"><?php print t('Zip Code'); ?></div>
+			  	<div class="be-view-value">88888</div>
+		  	</div>
+	  	</div>
+
+	  	<div class="col-xs-4">
+  			<div class="be-view-field">
+		  		<div class="be-view-label"><?php print t('Business Type'); ?></div>
+			  	<div class="be-view-value">SDVSOB</div>
+		  	</div>
+	  	</div>
+
+	  	<div class="col-xs-4">
+  			<div class="be-view-field">
+		  		<div class="be-view-label"><?php print t('Renewal Date'); ?></div>
+			  	<div class="be-view-value">01/01/2018</div>
+		  	</div>
+	  	</div>
+
+  	</div>
+
+  	<div class="be-view-field">
+  		<div class="be-view-label"><?php print t('Tags'); ?></div>
+	  	<div class="be-view-value">WECA, Conference 2017</div>
+  	</div>
+
+  </div>
 </div>
