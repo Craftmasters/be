a3180f3583cfc0f0e79edc3670e5a982e54b961e
Ruthie Hallarsis <ruthieborces@outlook.com>
2017-08-19 23:30:36 +0800

    WIP create event block

--- app/sites/all/modules/custom/bullseye_blocks/templates/be-create-event.tpl.php
+++ app/sites/all/modules/custom/bullseye_blocks/templates/be-create-event.tpl.php
@@ -1,3 +1,21 @@
 <div class="be-regular-block">
   <h2 class="be-regular-h2"><?php print t('Create Event'); ?></h2>
+  <div class="be-block-main">
+  	<div class="tabbed-block">
+      <div class="tab-navigations">
+        <ul class="tabs-menu">
+          <li class="current"><a href="#tab-0"><?php print t('Log Activity'); ?></a></li>
+          <li><a href="#tab-1"><?php print t('New Task'); ?></a></li>
+        </ul>
+      </div>
+      <div class="body-tabs">
+        <div id="tab-0" class="tab-content">
+          log activity
+        </div>
+        <div id="tab-1" class="tab-content">
+          new task
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
