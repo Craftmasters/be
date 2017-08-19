db2f72ceafafe4a5ac3da4fb2270bbea8d9ab223
Ruthie Hallarsis <ruthieborces@outlook.com>
2017-08-19 22:29:34 +0800

    Completed RFPs table in RPFs page

---
+++ app/sites/all/modules/custom/bullseye_blocks/templates/be-rfps.tpl.php
@@ -0,0 +1,62 @@
+<div class="be-table-block">
+  <div class="be-table-top-header">
+    <div class="row">
+      <div class="col-md-6">
+        <span class="account-count"><?php print t('All Open RFP\'s (6)'); ?></span>
+        <a class="be-table-button" href="#"><?php print t('Create New FRP'); ?></a>
+      </div>
+      <div class="col-md-6">
+        <div class="be-table-right-icons">
+          <a href="#"><img src="<?php print $magnifying_glass; ?>"></a>
+          <a href="#"><img src="<?php print $single_user_gray; ?>"></a>
+          <a href="#"><img src="<?php print $gray_gear; ?>"></a>
+          <a href="#"><img src="<?php print $three_bars; ?>"></a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="be-table-content">
+    <table class="be-tables">
+      <thead>
+        <tr>
+          <th class="cell-check"><input type="checkbox"></th>
+          <th><?php print t('RFP ID'); ?></th>
+          <th><?php print t('Account'); ?></th>
+          <th class="be-dot-td"><?php print t('Major Medical'); ?></th>
+          <th class="be-dot-td"><?php print t('Limited Medical'); ?></th>
+          <th class="be-dot-td"><?php print t('Teledoc'); ?></th>
+          <th class="be-dot-td"><?php print t('MEC'); ?></th>
+          <th class="be-dot-td"><?php print t('Dental'); ?></th>
+          <th class="be-dot-td"><?php print t('Vision'); ?></th>
+          <th class="be-dot-td"><?php print t('Life & AD&D'); ?></th>
+          <th class="be-dot-td"><?php print t('Short Term Dissability'); ?></th>
+          <th class="be-dot-td"><?php print t('Specialty Benefits'); ?></th>
+          <th><?php print t('Due Date'); ?></th>
+          <th><?php print t('Priority'); ?></th>
+          <th><?php print t('Created By'); ?></th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php for ($i = 0; $i < 2; $i++) : ?>
+          <tr>
+            <td class="cell-check"><input type="checkbox"></td>
+            <td><span class="orange-font">RFP366152</span></td>
+            <td><span class="gray-font">Massive Dynamic</span></td>
+            <td class="be-dot-td"><span class="dot-priority green"></td>
+            <td class="be-dot-td"><span class="dot-priority gray"></td>
+            <td class="be-dot-td"><span class="dot-priority green"></td>
+            <td class="be-dot-td"><span class="dot-priority gray"></td>
+            <td class="be-dot-td"><span class="dot-priority gray"></td>
+            <td class="be-dot-td"><span class="dot-priority green"></td>
+            <td class="be-dot-td"><span class="dot-priority green"></td>
+            <td class="be-dot-td"><span class="dot-priority gray"></td>
+            <td class="be-dot-td"><span class="dot-priority gray"></td>
+            <td><span class="light-gray-font">8/30/2017</span></td>
+            <td class="be-dot-td"><span class="dot-priority red"></span></td>
+            <td><span class="light-gray-font">James J.</span></td>
+          </tr>
+        <?php endfor; ?>
+      </tbody>
+    </table>
+  </div>
+</div>
