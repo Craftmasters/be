061a1109894e9520d48295b052120e9e91bef2b6
Ruthie Hallarsis <ruthieborces@outlook.com>
2017-08-23 17:29:31 +0800

    Added initial calendar page blocks

---
+++ app/sites/all/modules/custom/bullseye_blocks/templates/be-activities.tpl.php
@@ -0,0 +1,3 @@
+<div class="be-regular-block">
+  <h2 class="be-regular-h2"><?php print t('Activities'); ?></h2>
+</div>
---
+++ app/sites/all/modules/custom/bullseye_blocks/templates/be-calendar-activities.tpl.php
@@ -0,0 +1,3 @@
+<div class="be-regular-block">
+  <h2 class="be-regular-h2"><?php print t('Calendar'); ?></h2>
+</div>
