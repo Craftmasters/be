5955ee2abd606fa7b8ee4884241603fbce93bd32
Gerald Villorente <gerald@vielsoft.com>
2017-08-24 09:41:07 +0800

    WIP: Tons of changes in backoffice making tpls dynamic.

--- app/sites/all/themes/bullseye/template.php
+++ app/sites/all/themes/bullseye/template.php
@@ -236,7 +236,6 @@ function bullseye_preprocess_bullseye_rfp_form(&$vars) {
     $data = $_SESSION['rfp'];
     $account = node_load($data['account_id']);
 
-    krumo($account);
     $vars['company'] = $account->field_company[LANGUAGE_NONE][0]['value'];
     $vars['email'] = $account->field_email[LANGUAGE_NONE][0]['value'];
     $vars['phone'] = $account->field_work_phone[LANGUAGE_NONE][0]['value'];
