db7f6e41de186d85b75b6922322e7c21f13c85b1
Ruthie Hallarsis <ruthieborces@outlook.com>
2017-08-29 00:12:21 +0800

    Added initial current progress templates for leads

--- app/sites/all/modules/custom/bullseye_blocks/templates/be-current-progress.tpl.php
+++ app/sites/all/modules/custom/bullseye_blocks/templates/be-current-progress.tpl.php
@@ -1,3 +1,8 @@
 <div class="be-regular-block">
   <h2 class="be-regular-h2"><?php print t('Current Progress'); ?></h2>
+  <div class="be-block-main">
+  	<?php if ($cp_form) : ?>
+  		<?php print $cp_form; ?>
+  	<?php endif; ?>
+  </div>
 </div>
