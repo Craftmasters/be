5b665f404e94abdc9fc3c98e13906d9471dadc80
Voj Jeturian <jeturian@gmail.com>
2017-08-31 00:05:09 +0800

    finished validation of individual

--- app/sites/all/themes/bullseye/templates/forms/producer-acct.tpl.php
+++ app/sites/all/themes/bullseye/templates/forms/producer-acct.tpl.php
@@ -57,7 +57,7 @@ A few more things to wrap this up.</h4>
     </div>
   </div>
 
-  <div class="producer-acct-check">
+  <div class="producer-acct-check producer-acct-health-life">
     <div class="check-icon-gray"></div>
     <div class="producer-acct-file-field-wrapper">
       <label for="edit-file-health-life" class="producer-acct-file-label">Upload a copy of current Health and Life</label>
@@ -65,7 +65,7 @@ A few more things to wrap this up.</h4>
     </div>
   </div>
 
-  <div class="producer-acct-check">
+  <div class="producer-acct-check producer-acct-omission-insurance">
     <div class="check-icon-gray"></div>
     <div class="producer-acct-file-field-wrapper">
       <label for="edit-file-error-omission-insurance" class="producer-acct-file-label">Upload a copy of Errors and Omission Insurance</label>
@@ -86,11 +86,11 @@ $('#producer-acct-next-btn').click(function(e){
   e.preventDefault();
   // @todo: validate and check if required fields have been inputted and checked
   var inputs = $('#producer-acct-page-1').find('input.required');
+  var hasNoVal = false;
   if(inputs.length > 0){
-    var hasNoVal = false;
     inputs.each(function(index, value){
       input = $(value);
-      if(input.is(':checkbox') && input.is(':checked')){
+      if(input.is(':checkbox') && !input.is(':checked')){
         hasNoVal = true;
         return false;
       }
@@ -101,11 +101,64 @@ $('#producer-acct-next-btn').click(function(e){
     });
   }
 
-  if(!hasNoVal){
+  if(hasNoVal){
+    alert('Please complete and check all the fields.');
+  }
+  else{
     $('#producer-acct-page-1').hide();
     $('#producer-acct-page-2').show();
   }
   return false;
 });
+
+var hasAgreed = false;
+var hasHealthLife = false;
+var hasOmission = false;
+var agreements = $('#producer-acct-petition');
+var agreementCheck = $('.producer-acct-check.producer-acct-aggreement');
+agreements.click(function(e){
+  agreementCheck.children('.check-icon-gray').removeClass('check-icon-gray').addClass('check-icon-green');
+  hasAgreed = true;
+  return true;
+});
+var healthLife = $('#edit-file-health-life');
+var healthLifeCheck = $('.producer-acct-check.producer-acct-health-life');
+healthLife.change(function (e){
+  var file = $(this).val();
+  
+  if(file){
+    hasHealthLife = true;
+    healthLifeCheck.children('.check-icon-gray').removeClass('check-icon-gray').addClass('check-icon-green');
+  }
+  else{
+    hasHealthLife = false;
+  }
+});
+var omission = $('#edit-file-error-omission-insurance');
+var omissionCheck = $('.producer-acct-check.producer-acct-omission-insurance');
+omission.change(function (e){
+  var file = $(this).val();
+  
+  if(file){
+    hasOmission = true;
+    omissionCheck.children('.check-icon-gray').removeClass('check-icon-gray').addClass('check-icon-green');
+  }
+  else{
+    hasOmission = false;
+    hasMissingUpload++;
+  }
+});
+
+var form = $('#bullseye-producer-acct-indiv-form');
+form.submit(function(e) {
+  if(hasAgreed && hasHealthLife && hasOmission){
+    return true;
+  }
+  else{
+    e.preventDefault();
+    alert('Please see the link to the agreement and upload necessary documents.');
+  }
+});
+
 });  
 </script>
