950c1b5e8c60ec5ac938e76e78eccb849b82d728
Voj Jeturian <jeturian@gmail.com>
2017-08-31 00:58:59 +0800

    finished confirmation page

--- app/sites/all/themes/bullseye/templates/pages/page--producer-acct-confirm.tpl.php
+++ app/sites/all/themes/bullseye/templates/pages/page--producer-acct-confirm.tpl.php
@@ -1,7 +1,7 @@
 <div class="producer-acct-check">
-  <div class="check-icon-green"></div>
+  <div class="check-icon-gray"></div>
   <p>Thank you for your interest in Bullseye.</p>
   <p>Our verification team will review your application.</p>
-  <p>You’ll receive an email once your account
+  <p>You’ll receive an email once your account<br/>
 has been verified and activated.</p>
 </div>
