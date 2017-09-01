5d3d9182554468c84dc992380851d9c3ae84d6f0
Ruthie Hallarsis <ruthieborces@outlook.com>
2017-09-01 16:58:21 +0800

    Added initial current progress elements for opportunities page

---
+++ app/sites/all/modules/custom/bullseye_current_progress/templates/cp-opportunities-form.tpl.php
@@ -0,0 +1,116 @@
+<div class="current-progress-main">
+	
+	<div class="cp-step row gray-check">
+		<div class="col-xs-2">
+			<span class="indicator initial"></span>
+		</div>
+		<div class="col-xs-10">
+			<a href="#" class="cp-link big-step"><span><?php print t('Plan Specification'); ?></span></a>
+		</div>
+	</div>
+
+	<div class="cp-step row current-step">
+		<div class="col-xs-2">
+			<span class="indicator"></span>
+		</div>
+		<div class="col-xs-10">
+			<a href="#" class="cp-link">
+				<span><?php print t('Request specification'); ?></span>
+			</a>
+		</div>
+	</div>
+
+	<div class="cp-step row">
+		<div class="col-xs-2">
+			<span class="indicator"></span>
+		</div>
+		<div class="col-xs-10">
+			<a href="#" class="cp-link">
+				<span><?php print t('Receive plan details'); ?></span>
+			</a>
+		</div>
+	</div>
+
+	<div class="cp-step row gray-check">
+		<div class="col-xs-2">
+			<span class="indicator"></span>
+		</div>
+		<div class="col-xs-10">
+			<a href="#" class="cp-link big-step"><span><?php print t('RFP'); ?></span></a>
+		</div>
+	</div>
+
+	<div class="cp-step row">
+		<div class="col-xs-2">
+			<span class="indicator"></span>
+		</div>
+		<div class="col-xs-10">
+			<a href="#" class="cp-link">
+				<span><?php print t('Generate RFP'); ?></span>
+			</a>
+		</div>
+	</div>
+
+	<div class="cp-step row">
+		<div class="col-xs-2">
+			<span class="indicator"></span>
+		</div>
+		<div class="col-xs-10">
+			<a href="#" class="cp-link">
+				<span><?php print t('Send RFP'); ?></span>	
+			</a>
+		</div>
+	</div>
+
+	<div class="cp-step row">
+		<div class="col-xs-2">
+			<span class="indicator"></span>
+		</div>
+		<div class="col-xs-10">
+			<a href="#" class="cp-link">
+				<span><?php print t('Receive quote'); ?></span>
+			</a>
+		</div>
+	</div>
+
+	<div class="cp-step row gray-check">
+		<div class="col-xs-2">
+			<span class="indicator"></span>
+		</div>
+		<div class="col-xs-10">
+			<a href="#" class="cp-link big-step"><span><?php print t('Plan Presentation'); ?></span></a>
+		</div>
+	</div>
+
+	<div class="cp-step row">
+		<div class="col-xs-2">
+			<span class="indicator"></span>
+		</div>
+		<div class="col-xs-10">
+			<a href="#" class="cp-link">
+				<span><?php print t('Request information'); ?></span>
+			</a>
+		</div>
+	</div>
+
+	<div class="cp-step row">
+		<div class="col-xs-2">
+			<span class="indicator"></span>
+		</div>
+		<div class="col-xs-10">
+			<a href="#" class="cp-link">
+				<span><?php print t('Receive Feedback'); ?></span>
+			</a>
+		</div>
+	</div>
+
+	<div class="cp-step row no-check">
+		<div class="col-xs-2">
+			<span class="indicator end"></span>
+		</div>
+		<div class="col-xs-10">
+			<a href="#" class="cp-link big-step"><span><?php print t('Convert to Deals in Progress'); ?></span></a>
+		</div>
+	</div>
+
+</div>
