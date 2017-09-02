1a7105792daabe27d689527a6906125bb0785637
Voj Jeturian <jeturian@gmail.com>
2017-09-03 00:56:05 +0800

    added loop template for file directories

--- app/sites/all/themes/bullseye/templates/producer-file-directory-tree.tpl.php
+++ app/sites/all/themes/bullseye/templates/producer-file-directory-tree.tpl.php
@@ -3,12 +3,31 @@
  * template file for file directory tree looping
  */
 ?>
-<?php if(!empty($directories) && is_array($directories)):?>
-  <?php foreach($directories as $key => $value): ?>
+<?php
+function producer_file_process_directory($name){
+  ?>
+<?php if(!empty($name) && is_array($name)):?>
+  <?php foreach($name as $key => $value): ?>
     <?php if(is_string($value)): ?>
-      <div class="directory-name"><?php echo $value ?></div>
-    <?php else: ?>
-      <div class="directory-name has-children tree-open"><?php echo $value ?></div>
+      <li class="directory-name"><?php echo $value ?></li>
+    <?php elseif(is_array($value)): ?>
+      <li class="directory-name has-children tree-open">
+        <?php echo $key ?>
+        <ul class="directory-name-tree-container">
+          <?php producer_file_process_directory($value); ?>
+        </ul>
+      </li>
     <?php endif;?>
   <?php endforeach; ?>
 <?php endif;?>
+<?php
+}
+
+?>
+<ul class="directory-name-tree-container">
+  <li class="directory-name has-children tree-open">All Files
+    <ul class="directory-name-tree-container">
+  <?php producer_file_process_directory($directory); ?>
+    </ul>
+  </li>
+</ul>
