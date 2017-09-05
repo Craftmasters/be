5692b1979c7b25b509f4ccb2099cb0e6a446c6b1
Gerald Villorente <gerald@vielsoft.com>
2017-09-05 13:36:36 +0800

    WIP: Updated the tpl

--- app/sites/all/themes/bullseye/templates/forms/rfp-add-form.tpl.php
+++ app/sites/all/themes/bullseye/templates/forms/rfp-add-form.tpl.php
@@ -411,22 +411,36 @@
                   <div class="be-form-section">
                     <label><?php print t('Plan Year to Quote'); ?></label>
                     <div class="row">
-                      <div class="col-xs-6 quote-start"><?php print render($form['life_fields']['lf_plan_year_to_quote_start']); ?></div>
-                      <div class="col-xs-6 quote-end"><?php print render($form['life_fields']['lf_plan_year_to_quote_end']); ?></div>
+                      <div class="col-xs-6 quote-start">
+                        <?php print render($form['life_fields']['lf_plan_year_to_quote_start']); ?>
+                      </div>
+                      <div class="col-xs-6 quote-end">
+                        <?php print render($form['life_fields']['lf_plan_year_to_quote_end']); ?>
+                      </div>
                     </div>
                   </div>
 
-                  <div class="be-form-single renewal-plan"><?php print render($form['life_fields']['lf_renewal_of_current_plan']); ?></div>
+                  <div class="be-form-single renewal-plan">
+                    <?php print render($form['life_fields']['lf_renewal_of_current_plan']); ?>
+                  </div>
 
                   <div class="be-form-section">
-                    <label><?php print t('Percentage of Employer Contribution'); ?></label>
+                    <label>
+                      <?php print t('Percentage of Employer Contribution'); ?>
+                    </label>
                     <div class="row">
-                      <div class="col-xs-6"><?php print render($form['life_fields']['lf_percentage_single']); ?></div>
-                      <div class="col-xs-6"><?php print render($form['life_fields']['lf_percentage_family']); ?></div>
+                      <div class="col-xs-6">
+                        <?php print render($form['life_fields']['lf_percentage_single']); ?>
+                      </div>
+                      <div class="col-xs-6">
+                        <?php print render($form['life_fields']['lf_percentage_family']); ?>
+                      </div>
                     </div>
                   </div>
 
-                  <div class="be-form-single waiting-period"><?php print render($form['life_fields']['lf_waiting_period']); ?></div>
+                  <div class="be-form-single waiting-period">
+                    <?php print render($form['life_fields']['lf_waiting_period']); ?>
+                  </div>
                 </div>
               </div>
             <?php endif; ?>
