6f02a27ebc724eac82d527adb02a65715c5a9996
Gerald Villorente <gerald@vielsoft.com>
2017-09-14 23:49:35 +0800

    Dynamic total number of prospects in prospects listing

--- app/sites/all/modules/custom/bullseye_blocks/templates/be-prospects.tpl.php
+++ app/sites/all/modules/custom/bullseye_blocks/templates/be-prospects.tpl.php
@@ -2,7 +2,7 @@
   <div class="be-table-top-header">
     <div class="row">
       <div class="col-md-6">
-        <span class="account-count"><?php print t('All Prospects (6)'); ?></span>
+        <span class="account-count"><?php print t('All Prospects (' . $total . ')'); ?></span>
         <a class="be-table-button" href="/node/add/accounts?account_status=prospect" rel="lightframe"><?php print t('Add New'); ?></a>
       </div>
       <div class="col-md-6">
