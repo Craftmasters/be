1599ba7b464c47cd8931895a2895fd1c821612a2
Gerald Villorente <gerald@vielsoft.com>
2017-09-18 05:44:59 +0800

    Adjust the query for fetching opportunity account

--- app/sites/all/modules/custom/bullseye_api/bullseye_api.api.php
+++ app/sites/all/modules/custom/bullseye_api/bullseye_api.api.php
@@ -693,17 +693,16 @@ class Bullseye {
     }
     else {
       $query = db_select('node', 'n');
-      $query->leftJoin('field_data_field_firstname', 'fname', 'n.nid = fname.entity_id');
-      $query->leftJoin('field_data_field_middle_name', 'mname', 'n.nid = mname.entity_id');
-      $query->leftJoin('field_data_field_lastname', 'lname', 'n.nid = lname.entity_id');
-      $query->leftJoin('field_data_field_prefix', 'pfix', 'n.nid = pfix.entity_id');
-      $query->leftJoin('field_data_field_title', 'title', 'n.nid = title.entity_id');
-      $query->leftJoin('field_data_field_company', 'comp', 'n.nid = comp.entity_id');
-      $query->leftJoin('field_data_field_email', 'mail', 'n.nid = mail.entity_id');
       $query->leftJoin('field_data_field_source', 'source', 'n.nid = source.entity_id');
       $query->leftJoin('field_data_field_type_of_business', 'btype', 'n.nid = btype.entity_id');
       $query->leftJoin('field_data_field_account_status', 'type', 'n.nid = type.entity_id');
       $query->leftJoin('field_data_field_workflow_status', 'w', 'n.nid = w.entity_id');
+      $query->leftJoin('field_data_field_contacts', 'contact', 'n.nid = contact.entity_id');
+      $query->leftJoin('field_data_field_firstname', 'fname', 'contact.entity_id = fname.entity_id');
+      $query->leftJoin('field_data_field_middle_name', 'mname', 'contact.entity_id = mname.entity_id');
+      $query->leftJoin('field_data_field_lastname', 'lname', 'contact.entity_id = lname.entity_id');
+      $query->leftJoin('field_data_field_email', 'mail', 'contact.entity_id = mail.entity_id');
+      $query->leftJoin('field_data_field_position', 'pos', 'contact.field_contacts_value = pos.entity_id');
       $accounts = $query
         ->fields('n', array('nid'))
         ->fields('type', array('field_account_status_value'))
@@ -711,11 +710,10 @@ class Bullseye {
         ->fields('fname', array('field_firstname_value'))
         ->fields('mname', array('field_middle_name_value'))
         ->fields('lname', array('field_lastname_value'))
-        ->fields('comp', array('field_company_value'))
         ->fields('mail', array('field_email_value'))
         ->fields('source', array('field_source_value'))
         ->fields('btype', array('field_type_of_business_value'))
-        ->fields('title', array('field_title_value'))
+        ->fields('pos', array('field_position_value'))
         ->condition('n.type', 'accounts', '=')
         ->condition('n.status', 1, '=')
         ->condition('type.field_account_status_value', 'opportunity', '=')
