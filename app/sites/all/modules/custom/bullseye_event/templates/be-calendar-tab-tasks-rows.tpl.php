c5bb63a11b42baf7748193595ccaf7e9cc09a1b5
Ruthie Hallarsis <ruthieborces@outlook.com>
2017-10-03 21:57:46 +0800

    Adjusted styling for tasks block in calendar tab

--- app/sites/all/modules/custom/bullseye_event/templates/be-calendar-tab-tasks-rows.tpl.php
+++ app/sites/all/modules/custom/bullseye_event/templates/be-calendar-tab-tasks-rows.tpl.php
@@ -1,16 +1,22 @@
-<?php foreach($tasks as $key => $value) : ?>
-	<div class="be-task-row row <?php print $value->field_task_status_value; ?>-task">
-		<div class="col-xs-1">
-			<input type="checkbox" value="<?php print $value->nid; ?>" class="task-checkbox" <?php print $value->checkbox_disabled; ?>>
-		</div>
-		<div class="col-xs-5">
-			<span class="green-font task-desc <?php print $value->dot; ?>"><?php print $value->description; ?></span>
-		</div>
-		<div class="col-xs-3">
-			<span class="gray-font">by <?php print $value->contact_name; ?></span>
-		</div>
-		<div class="col-xs-3 task-date">
-			<span class="blue-gray-font">Due <?php print $value->formatted_date; ?></span>
-		</div>  	
-  </div>
-<?php endforeach; ?>
+<?php if (!empty($tasks)) : ?>
+	<?php foreach($tasks as $key => $value) : ?>
+		<div class="be-task-row row <?php print $value->field_task_status_value; ?>-task">
+			<div class="col-xs-1">
+				<input type="checkbox" value="<?php print $value->nid; ?>" class="task-checkbox" <?php print $value->checkbox_disabled; ?>>
+			</div>
+			<div class="col-xs-5">
+				<span class="green-font task-desc <?php print $value->dot; ?>"><?php print $value->description; ?></span>
+			</div>
+			<div class="col-xs-3">
+				<span class="gray-font">by <?php print $value->contact_name; ?></span>
+			</div>
+			<div class="col-xs-3 task-date">
+				<span class="blue-gray-font">Due <?php print $value->formatted_date; ?></span>
+			</div>  	
+	  </div>
+	<?php endforeach; ?>
+<?php else : ?>
+	<div class="be-block-no-content">
+		<span class="no-content"><?php print t('No content available at this moment.'); ?></span>
+	</div>
+<?php endif; ?>
