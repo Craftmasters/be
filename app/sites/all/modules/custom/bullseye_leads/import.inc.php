dfe495fcad0ee32061edb7624ef1bc7d579c9c84
Gerald Villorente <gerald@vielsoft.com>
2017-10-05 16:11:51 +0800

    Modified the import helper function to do some extra validation before imports

--- app/sites/all/modules/custom/bullseye_leads/import.inc.php
+++ app/sites/all/modules/custom/bullseye_leads/import.inc.php
@@ -71,27 +71,72 @@ function bullseye_leads_import_leads_submit($form, &$form_state) {
 
   $rows = array();
 
+  $columns = array(
+    'Type',
+    'First Name',
+    'Middle Initial',
+    'Last Name',
+    'Prefix',
+    'Suffix',
+    'Title',
+    'Details',
+    'Value',
+    'Mandate Category',
+    'Company',
+    'Type of Business',
+    'Owned By',
+    'Source',
+    'Street',
+    'City',
+    'State',
+    'State Code',
+    'Postal Code',
+    'Work Phone',
+    'Mobile Phone',
+    'Email',
+    'Work Website',
+    'Personal Website',
+    'LinkedIn Personal',
+    'Facebook Personal',
+    'Tags',
+    'Producers',
+    'Referral Partners',
+    'Account Status',
+    'Workflow Status',
+    'Action Status',
+  );
+
+  $error = TRUE;
+
   if ($csv_file = file_load($form_state['values']['csv_file'])) {
     if (($handle = fopen($csv_file->uri, "r")) !== FALSE) {
       while (($data = fgetcsv($handle, NULL, ",")) !== FALSE) {
         // Validate the columns of the csv to make sure that the file
         // adhere to the template.
-        if (count($data) != 31) {
+        if (count($data) == 32) {
+          if ($data[0] && Bullseye::validAccountCsv($columns, $data)) {
+            $error = FALSE;
+          }
           array_push($rows, $data);
         }
         else {
-          $message = t('ERROR: The CSV is invalid. Please make sure that the csv is compliant to the systems requirements.');
-          drupal_set_message($message, 'error');
-          header("Refresh:0; url=/accounts");
-          break;
+          $error = TRUE;
         }
       }
     }
   }
 
-  if ($rows) {
+  if (!$error && $rows) {
     // Remove the header row from the array.
     array_shift($rows);
     _import_account($rows, $type);
+
+    // Notify the user once the import is done.
+    drupal_set_message(t('Import successfully completed.'), 'status');
+  }
+  else {
+    $message = t('ERROR: The CSV is invalid. Please make sure that the csv is compliant to the systems requirements.');
+    drupal_set_message($message, 'error');
+    header("Refresh:0; url=/accounts");
   }
 }
